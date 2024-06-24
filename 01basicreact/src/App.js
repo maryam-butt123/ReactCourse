b150a716f0d89990b1ffa4e07d58a64d5d1bc5a2
maryam-butt123 <mbutt5746@gmail.com>
2024-06-23 21:28:53 -0700

    created-simple-component-in-both

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,12 @@
+import Chai from "./Chai";
 
 function App() {
   return (
+    <>
     <h1>hello</h1>
-
-  );
+    <Chai/>
+    </>
+    );
 }
 
 export default App;
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,11 @@
+import Chai from './Chai'
+
 function App() {
   return (
+    <>
     <h1> hello</h1>
+    <Chai/>
+    </>
   )
 }
 
