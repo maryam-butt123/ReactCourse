0963ccf11b0c7f02f07780e3dc94b5b8233898d8
maryam-butt123 <mbutt5746@gmail.com>
2024-06-24 03:26:21 -0700

    built-custom-react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
 import Chai from './Chai'
 
 function App() {
+  const username="Maryam Butt"
   return (
     <>
-    <h1> hello</h1>
+    {/* saywe're returning this component to main.jsx now this willbe converted in that syntax in the main.jsx defined. (reactElement object) */}
+    {/* evaluated expression in {} */}
+     <h1>Hello {username}</h1>  
     <Chai/>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//we can execute a method as well just like <MyApp/>
+function MyApp(){
+  return (
+    <h1>This is custom MyApp</h1>
+  )
+}
+
+const anotherElement=
+(
+  <a href="https:google.com" target='_blank'>Click me</a>
 )
+
+
+const username=' Mary'
+//creating elements through React and then rendering them.
+//this kinda object injected directly and syntax is an object which has a type,properties/attributes,children/name/text,evaluated expressions {} of JS 
+const reactElement=React.createElement(
+  'a',
+  {
+    href:'https://google.com',
+    target:'_blank'
+  },
+  'Click me to visit Google',
+  username
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+reactElement)
+ 
