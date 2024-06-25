44724d5bf182b0ef718df0641a7434e823362651
maryam-butt123 <mbutt5746@gmail.com>
2024-06-24 23:04:46 -0700

    counter-interview-ques

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -24,11 +24,24 @@ function App() {
 //SECOND METHOD:
 
   const AddValue=() =>{
-    if(counter>=20)
-      {
-        return
-      }
-      setCounter(prevCounter=>prevCounter+1)
+    // if(counter>=20)
+    //   {
+    //     return
+    //   }
+    //   setCounter(prevCounter=>prevCounter+1)
+    
+    //if we do this, what willbe the output? will it add 4 everytime? 
+    // setCounter(counter+1)
+    //    setCounter(counter+1)
+    //    setCounter(counter+1)
+    //    setCounter(counter+1)
+       //THE ANSWER IS NO, CUZ THE USE STATE SENDS THE CHANGES TO THE UI AND VARIABLES IN BATCHES AND THESE 4 THINGS ABOVE MAKE A BATCH AND THEY'RE UPDATING THE SAME COUNTER VARIABLE EVERYTIME,SO IF WE HAVE TO UPDATE THE STATE LIKE THIS BY REPEATING EVERYTIME THEN WE HAVE TO DO THIS:
+       setCounter(prevCounter=>prevCounter+1)
+       setCounter(prevCounter=>prevCounter+1)
+       setCounter(prevCounter=>prevCounter+1)
+       setCounter(prevCounter=>prevCounter+1)
+       //NOW THE SETCOUNTER OR ALL SETTERS ACCEPT ONE CALLBACK AND PREVCOUNTER HERE GIVES THE LAST UPDATED STATE
+
 }
   const RemoveValue=()=>{
     if(counter<=0)
